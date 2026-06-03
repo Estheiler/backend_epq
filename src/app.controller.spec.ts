b29cbe55fd07246b24843d817a549c2d8de7e2eb
Estheiler <107735346+Estheiler@users.noreply.github.com>
2026-06-03 14:51:26 -0500

    test: actualizar saludo inicial de la API y pruebas

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "API EPQ en línea - Version 1.0"', () => {
+      expect(appController.getHello()).toBe('API EPQ en línea - Version 1.0');
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'API EPQ en línea - Version 1.0';
   }
 }
